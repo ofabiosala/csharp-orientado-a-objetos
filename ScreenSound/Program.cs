ca79ff30d26ab963dc2fd8d7f49c531b698a6c62
Fábio Sala <ofabiosala@gmail.com>
2024-08-17 10:06:24 -0300

    (refactor): implementação do método "ShowMusicInfo()".

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -8,15 +8,4 @@ musicOne.year = 1965;
 musicOne.duration = 123;
 musicOne.active = true;
 
-Console.WriteLine($"{musicOne.title} - {musicOne.artist}");
-Console.WriteLine($"{musicOne.album} ({musicOne.year}) - {musicOne.genre}");
-Console.WriteLine($"{musicOne.duration} segundos");
-
-if (musicOne.active)
-{
-    Console.WriteLine("Ativa");
-}
-else
-{
-    Console.WriteLine("Inativa");
-}
+musicOne.ShowMusicInfo();
