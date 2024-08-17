ba365218ebd70443cb92c91cee373ae734358835
Fábio Sala <ofabiosala@gmail.com>
2024-08-17 10:01:00 -0300

    (feat): Instância de um novo objeto do tipo "Music".

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Music musicOne = new Music();
+
+musicOne.title = "Yesterday";
+musicOne.artist = "The Beatles";
+musicOne.genre = "Rock";
+musicOne.album = "Help!";
+musicOne.year = 1965;
+musicOne.duration = 123;
+musicOne.active = true;
+
+Console.WriteLine($"{musicOne.title} - {musicOne.artist}");
+Console.WriteLine($"{musicOne.album} ({musicOne.year}) - {musicOne.genre}");
+Console.WriteLine($"{musicOne.duration} segundos");
+
+if (musicOne.active)
+{
+    Console.WriteLine("Ativa");
+}
+else
+{
+    Console.WriteLine("Inativa");
+}
