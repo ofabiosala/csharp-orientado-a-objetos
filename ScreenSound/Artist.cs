e76cabbbc793a296c258beb7b43f9d6e0af1a3c3
Fábio Sala <ofabiosala@gmail.com>
2024-08-22 12:09:35 -0300

    (feat): adicionada classe "Artist".

---
+++ ScreenSound/Artist.cs
@@ -0,0 +1,26 @@
+﻿class Artist
+{
+    public Artist(string title)
+    {
+        Title = title;
+    }
+
+    private List<Album> albuns = new List<Album>();
+    public string Title { get; set; }
+
+    public void AddAlbum(Album album)
+    {
+        albuns.Add(album);
+    }
+
+    public void ShowAlbuns()
+    {
+        Console.WriteLine($"Discografia - {Title}");
+
+        foreach (Album album in albuns)
+        {
+            Console.WriteLine($"- {album.Title}");
+        }
+
+    }
+}
